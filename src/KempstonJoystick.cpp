107177479539ba3477a424a070b0b07368d4c1a5
Adrian Conlon <Adrian.conlon@gmail.com>
2019-11-12 22:39:13 +0000

    Make the KempstonJoystick class a little easier to understand.
    Signed-off-by: Adrian Conlon <Adrian.conlon@gmail.com>

--- src/KempstonJoystick.cpp
+++ src/KempstonJoystick.cpp
@@ -11,41 +11,41 @@ KempstonJoystick::KempstonJoystick(Board& motherboard)
 }
 
 void KempstonJoystick::pushUp() {
-	m_contents |= Up;
+	m_contents = EightBit::Chip::setBit(m_contents, Up);
 }
 
 void KempstonJoystick::pushDown() {
-	m_contents |= Down;
+	m_contents = EightBit::Chip::setBit(m_contents, Down);
 }
 
 void KempstonJoystick::pushLeft() {
-	m_contents |= Left;
+	m_contents = EightBit::Chip::setBit(m_contents, Left);
 }
 
 void KempstonJoystick::pushRight() {
-	m_contents |= Right;
+	m_contents = EightBit::Chip::setBit(m_contents, Right);
 }
 
 void KempstonJoystick::pushFire() {
-	m_contents |= Fire;
+	m_contents = EightBit::Chip::setBit(m_contents, Fire);
 }
 
 void KempstonJoystick::releaseUp() {
-	m_contents &= ~Up;
+	m_contents = EightBit::Chip::clearBit(m_contents, Up);
 }
 
 void KempstonJoystick::releaseDown() {
-	m_contents &= ~Down;
+	m_contents = EightBit::Chip::clearBit(m_contents, Down);
 }
 
 void KempstonJoystick::releaseLeft() {
-	m_contents &= ~Left;
+	m_contents = EightBit::Chip::clearBit(m_contents, Left);
 }
 
 void KempstonJoystick::releaseRight() {
-	m_contents &= ~Right;
+	m_contents = EightBit::Chip::clearBit(m_contents, Right);
 }
 
 void KempstonJoystick::releaseFire() {
-	m_contents &= ~Fire;
+	m_contents = EightBit::Chip::clearBit(m_contents, Fire);
 }
