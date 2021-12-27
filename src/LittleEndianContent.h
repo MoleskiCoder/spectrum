7c1b2de9b904322225c360857360e4d8e88e4087
Adrian Conlon <adrian.conlon@gmail.com>
2021-12-27 10:35:13 +0000

    It may well be handy to derive more specific loaders from the "Content" classes.

--- src/LittleEndianContent.h
+++ src/LittleEndianContent.h
@@ -2,7 +2,7 @@
 
 #include "Content.h"
 
-class LittleEndianContent final : public Content {
+class LittleEndianContent : public Content {
 public:
 	[[nodiscard]] EightBit::register16_t readWord(int position) final;
 };
